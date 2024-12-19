8f8d22423831821bb8477560a92d48303fdbd87e
Sesi Kalyan <kalyanmaram174@gmail.com>
2024-12-19 18:38:47 +0530

    Weather application

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WeatherApi from "./WeatherApp";
+export default class App extends React.Component{
+    render(){
+        return (
+        <>
+        <WeatherApi/>
+        </>
+        );
+    }
+}
